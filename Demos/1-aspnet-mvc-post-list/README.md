eda325c1d8a612d06b3736a31de345d19400343e
russell walker <russ.h.walker@gmail.com>
2019-07-30 13:47:59 -0400

    adding initial two base

---
+++ Demos/1-aspnet-mvc-post-list/README.md
@@ -0,0 +1,15 @@
+# 1-aspnet-mvc-post-list
+
+## Simple String List
+1. Model Setup
+2. Setup viewmodel.
+3. Setup controller.
+4. Setup View
+5. Post
+
+## Complex Object List
+1. Model Setup
+2. Setup viewmodel.
+3. Setup controller.
+4. Setup View
+5. Post
---
+++ Demos/1-aspnet-mvc-post-list/src/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult KidNames()
+        {
+            var viewModel = new Models.KidNamesViewModel
+            {
+                KidNames = new List<string>
+                {
+                    "Pete",
+                    "Jane"
+                }
+            };
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult KidNames(Models.KidNamesViewModel viewModel)
+        {
+            //TODO save logic here
+            return View(viewModel);
+        }
+
+        public ActionResult Kids()
+        {
+            var viewModel = new Models.KidsViewModel
+            {
+                Kids = new List<Models.Kid>
+                {
+                    new Models.Kid
+                    {
+                        Id = 1,
+                        Name = "Pete Doe",
+                        Age = 7
+                    },
+                    new Models.Kid
+                    {
+                        Id = 2,
+                        Name = "Jane Johnson",
+                        Age = 3
+                    }
+                }
+            };
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Kids(Models.KidsViewModel viewModel)
+        {
+            //TODO save logic here
+            return View(viewModel);
+        }
+    }
+}
